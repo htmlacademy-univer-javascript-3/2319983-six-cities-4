529b72643535eadc334ed195dc627f0b53ae41f8
Bepuro <yazov.nik.yandex.ru>
2024-03-13 04:04:31 +0500

    router

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,47 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../../pages/main/main";
 import { ListProps } from "../main/list-places/list-places";
+import { AppRoute, AuthorizationStatus } from "../../const";
+import Login from "../../pages/login/login";
+import Favorite from "../../pages/favorites/favorites";
+import Offer from "../../pages/offer/offer";
+import NotFound from "../../pages/not-found/not-found";
+import PrivateRoute from "../common/private-route/private-route";
  
 
 function App({places}: ListProps): JSX.Element {
     return(
-        <Main places = {places}/>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                path = {AppRoute.Main}
+                element =  {<Main places = {places}/>}
+                />
+                <Route
+                path= {AppRoute.Login}
+                element = {<Login/>}
+                />
+                <Route
+                path= {AppRoute.Favorite}
+                element = {
+                    <PrivateRoute
+                        authorizationStatus={AuthorizationStatus.NoAuth}
+                    >
+                        <Favorite/>
+                    </PrivateRoute> 
+                }
+                />
+                <Route
+                path= {AppRoute.Offer}
+                element = {<Offer/>}
+                />
+                <Route
+                    path="*"
+                    element={<NotFound />}
+                />
+            </Routes>
+        </BrowserRouter>
+        
     );
 }
 
